1828fe3f69bedbd5729a03ea19497e42de80ec64
toninikolaev0707-bit <toni.nikolaev.0707@mail.ru>
2026-03-31 09:46:41 +0300

    Update InitiativeListItem.tsx

--- src/zipperInitiative/InitiativeListItem.tsx
+++ src/zipperInitiative/InitiativeListItem.tsx
@@ -102,8 +102,9 @@ export function InitiativeListItem({
         })}
       >
         <IconButton
+          
           sx={{ paddingX: 0, paddingY: 0, height: 60, width: 60 }}
-          onClick={() => removeFromInitiative(item.id)}
+          onClick={() => {if (!roleIsGm) return; removeFromInitiative(item.id);}}
           tabIndex={-1}
           onDoubleClick={(e) => e.stopPropagation()}
         >
